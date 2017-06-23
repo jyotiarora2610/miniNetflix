2baffbcf3014bbf940be23fd19adc2e9a33012b2
jyotiarora <jyoti.arora@lifcare.in>
2017-06-23 16:07:22 +0530

    minor fix

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -6,7 +6,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = 'EeobY3jxsMoFREmqfmsZwAALQb73WeWm';
 const CLIENT_DOMAIN = 'unicoder.auth0.com';
-const REDIRECT = 'http://localhost:3000/callback';
+const REDIRECT = '/callback';
 const SCOPE = 'full:access';
 const AUDIENCE = 'http://miniflix.com';
 

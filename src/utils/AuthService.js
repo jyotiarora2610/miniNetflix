d935f3622311c350a4d51d44e1c1c49bd780a120
jyotiarora <jyoti.arora@lifcare.in>
2017-06-23 16:12:28 +0530

    prod change

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -6,7 +6,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = 'EeobY3jxsMoFREmqfmsZwAALQb73WeWm';
 const CLIENT_DOMAIN = 'unicoder.auth0.com';
-const REDIRECT = '/callback';
+const REDIRECT = ' https://miniflix.herokuapp.com/callback';
 const SCOPE = 'full:access';
 const AUDIENCE = 'http://miniflix.com';
 
